d9ba38aa160a06b0f76aaf61aa23a7e9b8d1bb3a
Guangchuang Yu <guangchuangyu@gmail.com>
2015-05-25 22:53:24 +0800

    Archie

---
+++ BJP3/Chapter1/Archie.java
@@ -0,0 +1,8 @@
+public class Archie {
+    public static void main(String[] args) {
+	System.out.println("name\tage\theight");
+	System.out.println("Archie\t17\t5'9\"");
+	System.out.println("Betty\t17\t5'6\"");
+	System.out.println("Jughead\t16\t6'");
+    }
+}
--- BJP3/Chapter1/README.md
+++ BJP3/Chapter1/README.md
@@ -4,3 +4,4 @@
 * 1.1 binaryNumbers
 * 1.2 [binary1](binary1.java)
 * 1.3 [binary2](binary2.java)
+* 1.9 [Archie](Archie.java)
