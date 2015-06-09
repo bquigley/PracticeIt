ab2caa78f2d9c62e1f369813ffaea96777372705
GuangchuangYu <guangchuangyu@gmail.com>
2015-06-09 22:10:59 +0800

    start supplement3G

---
+++ BJP3/Supplement3G/MickeyBox.java
@@ -0,0 +1,31 @@
+/*
+
+
+Write a complete program in a class named MickeyBox that uses the DrawingPanel to draw the following figure:
+Java DrawingPanel close maximize minimize
+expected output
+(201, 37)
+
+The window is 220 pixels wide and 150 pixels tall. The background is yellow. There are two blue ovals of size 40 x 40 pixels. The left oval's top-left corner is located at position (50, 25), and the two ovals' top-left corners are 80 pixels apart horizontally. There is a red square whose top two corners exactly intersect the centers of the two ovals. Lastly, there is a black horizontal line through the center of the square.
+
+(You don't need to include any import statements at the top of your program.) (The next exercise is a modified version of this program, so you can use the code you write here as a starting point for that exercise.)
+*/
+
+import java.awt.*;
+
+public class MickeyBox{
+    public static void main(String[] args) {
+	DrawingPanel panel = new DrawingPanel(220, 150);
+	Graphics g = panel.getGraphics();
+	g.setColor(Color.YELLOW);
+	g.fillRect(0, 0, 220, 150);
+	g.setColor(Color.BLUE);
+	g.fillOval(50, 25, 40, 40);
+	g.fillOval(130, 25, 40, 40);
+	g.setColor(Color.RED);
+	g.fillRect(70, 45, 80, 80);
+	g.setColor(Color.BLACK);
+	g.drawLine(70, 85, 150, 85);
+    }
+
+}
---
+++ BJP3/Supplement3G/README.md
@@ -0,0 +1,8 @@
+# Building Java Program, 3rd edition
+
+## Supplement 3G: Graphics
+
+To solve problems, we should download the [DrawingPanel.java](DrawingPanel.jav) from [http://courses.cs.washington.edu/courses/cse142/12wi/labs/problems/DrawingPanel.java](http://courses.cs.washington.edu/courses/cse142/12wi/labs/problems/DrawingPanel.java).
+
+### Excercise
+* 3G.1 [MickeyBox](MickeyBox.java)
